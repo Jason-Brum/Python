9c1962ba9929354b2e880f6f8e1ba4e01b059eb2
Jason-Brum <contato@jasonb.com.br>
2024-10-30 16:40:40 -0300

    ok

---
+++ exercicios_faculdade/semana_04/exemplo_13.py
@@ -0,0 +1,16 @@
+frutas = ["banana", "pera", "laranja", "mamão", "goiaba", "manga", "abacate"]
+for i, item in enumerate(frutas):
+    print(i, item)
+
+print ("--------------")    
+
+nums = [12, 1, 4, 9, 17, 0, 3]    
+for i, numeros in enumerate(nums):
+    print(i, numeros)
+
+print ("--------------")      
+
+frutas = ["banana", "pera", "laranja", "mamão", "goiaba", "manga", "abacate"]
+for i, item in enumerate(frutas):
+    if i < 4:
+        print(i, item)    

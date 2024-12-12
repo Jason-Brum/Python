a5f02532684a043ee8aef8f64294419671d1467e
Jason-Brum <contato@jasonb.com.br>
2024-12-12 12:14:31 -0300

    ok

---
+++ CRUD/crud_08/teste.py
@@ -0,0 +1,26 @@
+def insert1(*args):
+    global lista;
+for in in args:
+    lista.append(i)
+
+def insert2 (lista, item):
+    lista.append(i)
+    return lista
+
+def insert3 (lista, key, item):
+    lista[key] = item
+    return lista
+
+def delet (lista, key):
+    del lista [key]
+    return lista
+
+def update (lista, key, item):
+    del lista [key]
+    return lista   
+
+lista = ["maça", "laranja", "abacate"]
+
+# Nesta linha foi removida a chamada da função
+
+print(lista)
---
+++ CRUD/crud_08/teste2.py
@@ -0,0 +1,10 @@
+val= 0
+
+while val<5:
+    print(val)
+    val = val + 1
+
+for i in[1, 2, 3,4]:
+    val = val + 2
+
+print(val)        
